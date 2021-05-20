649c5fff341f55fccbe209f9e29954d6445c5095
Hossain Rabbi <hossainrabbi2k1@gmail.com>
2021-05-20 18:37:29 +0600

    make services json data

---
+++ src/data/services.data.js
@@ -0,0 +1,26 @@
+export const services = [
+    {
+        id: 1,
+        icon: '',
+        title: 'Faster Delivry',
+        info: 'Food at your under 30 minutes',
+    },
+    {
+        id: 2,
+        icon: '',
+        title: 'Special Carriage',
+        info: 'Your food as warm as it was cooked',
+    },
+    {
+        id: 3,
+        icon: '',
+        title: 'Cheaper Delivry',
+        info: 'Just pay for food, not for the ride',
+    },
+    {
+        id: 4,
+        icon: '',
+        title: 'Chilivery Support',
+        info: '24/7 support and live chat with our team',
+    },
+];
